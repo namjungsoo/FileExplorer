084aac41dd6f91b51f0f8fb914ac1b8d3e137ab6
Jungsoo Nam <namjungsoo@gmail.com>
2016-12-04 17:02:18 +0900

    fragment 분리

---
+++ app/src/main/java/com/duongame/fileexplorer/fragment/ExplorerFragment.java
@@ -0,0 +1,317 @@
+package com.duongame.fileexplorer.fragment;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.HorizontalScrollView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
+
+import com.duongame.fileexplorer.R;
+import com.duongame.fileexplorer.activity.PhotoActivity;
+import com.duongame.fileexplorer.activity.ZipActivity;
+import com.duongame.fileexplorer.adapter.ExplorerAdapter;
+import com.duongame.fileexplorer.adapter.ExplorerFileItem;
+import com.duongame.fileexplorer.adapter.ExplorerGridAdapter;
+import com.duongame.fileexplorer.adapter.ExplorerListAdapter;
+import com.duongame.fileexplorer.bitmap.BitmapCacheManager;
+import com.duongame.fileexplorer.helper.ExplorerSearcher;
+import com.duongame.fileexplorer.helper.PositionManager;
+import com.duongame.fileexplorer.helper.PreferenceHelper;
+
+import java.util.ArrayList;
+
+/**
+ * Created by namjungsoo on 2016-11-23.
+ */
+
+public class ExplorerFragment extends Fragment {
+    private final static String TAG = "ExplorerFragment";
+
+    private final static int MAX_THUMBNAILS = 20;
+    private final static int SWITCH_LIST = 0;
+    private final static int SWITCH_GRID = 1;
+
+    private int viewType = SWITCH_LIST;
+
+    private ExplorerAdapter adapter;
+    private ArrayList<ExplorerFileItem> fileList;
+    private TextView textPath;
+    private HorizontalScrollView scrollPath;
+    private GridView gridView;
+    private ListView listView;
+    private AbsListView currentView;
+    private ViewSwitcher switcher;
+    private View rootView;
+
+
+    @Nullable
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             Bundle savedInstanceState) {
+        //setContentView(R.layout.fragment_explorer);
+        rootView = inflater.inflate(R.layout.fragment_explorer, container, false);
+
+        initUI();
+        initViewType();
+
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onPause");
+
+        // 밖에 나갔다 들어오면 리프레시함
+        updateFileList(ExplorerSearcher.getLastPath());
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        final int position = currentView.getFirstVisiblePosition();
+        final int top = getCurrentViewScrollTop();
+        Log.d(TAG, "onPause position=" + position + " top="+top);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                PreferenceHelper.setLastPosition(getActivity(), position);
+                PreferenceHelper.setLastTop(getActivity(), top);
+            }
+        }).start();
+    }
+
+    void initUI() {
+        switcher = (ViewSwitcher) rootView.findViewById(R.id.switcher);
+        textPath = (TextView) rootView.findViewById(R.id.text_path);
+        scrollPath = (HorizontalScrollView) rootView.findViewById(R.id.scroll_path);
+        fileList = new ArrayList<>();
+
+        final Button view = (Button) rootView.findViewById(R.id.btn_view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button btn = (Button) view;
+                if (viewType == SWITCH_LIST) {
+                    switchToGrid();
+                    PreferenceHelper.setViewType(getActivity(), SWITCH_GRID);
+                } else {
+                    switchToList();
+                    PreferenceHelper.setViewType(getActivity(), SWITCH_LIST);
+                }
+            }
+        });
+
+        final Button home = (Button) rootView.findViewById(R.id.btn_home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateFileList(null);
+            }
+        });
+
+        final Button up = (Button) rootView.findViewById(R.id.btn_up);
+        up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUpDirectory();
+            }
+        });
+    }
+
+    void initViewType() {
+        int viewType = PreferenceHelper.getViewType(getActivity());
+        switch (viewType) {
+            case SWITCH_LIST:
+                switchToList();
+
+                break;
+            case SWITCH_GRID:
+                switchToGrid();
+                break;
+        }
+    }
+
+    void switchToList() {
+        switcher.setDisplayedChild(SWITCH_LIST);
+
+        adapter = new ExplorerListAdapter(getActivity(), fileList);
+
+        listView = (ListView) rootView.findViewById(R.id.list_explorer);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                onAdapterItemClick(position);
+            }
+        });
+
+        final Button view = (Button) rootView.findViewById(R.id.btn_view);
+        view.setText("Grid");
+
+        viewType = SWITCH_LIST;
+        currentView = listView;
+
+        moveToSelection(ExplorerSearcher.getLastPath());
+    }
+
+    void switchToGrid() {
+        switcher.setDisplayedChild(SWITCH_GRID);
+
+        adapter = new ExplorerGridAdapter(getActivity(), fileList);
+
+        gridView = (GridView) rootView.findViewById(R.id.grid_explorer);
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                onAdapterItemClick(position);
+            }
+        });
+
+        final Button view = (Button) rootView.findViewById(R.id.btn_view);
+        view.setText("List");
+
+        viewType = SWITCH_GRID;
+        currentView = gridView;
+
+        moveToSelection(ExplorerSearcher.getLastPath());
+    }
+
+    void refreshThumbnail(String path) {
+        ArrayList<ExplorerFileItem> imageList = ExplorerSearcher.getImageList();
+        for (ExplorerFileItem item : imageList) {
+            getActivity().sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + item.path)));
+        }
+    }
+
+    public void gotoUpDirectory() {
+        String path = ExplorerSearcher.getLastPath();
+        path = path.substring(0, path.lastIndexOf('/'));
+        if (path.length() == 0) {
+            path = "/";
+        }
+        PositionManager.setPosition(ExplorerSearcher.getLastPath(), currentView.getFirstVisiblePosition());
+        PositionManager.setTop(ExplorerSearcher.getLastPath(), getCurrentViewScrollTop());
+
+        updateFileList(path);
+    }
+
+    void onAdapterItemClick(int position) {
+        ExplorerFileItem item = fileList.get(position);
+        switch (item.type) {
+            case DIRECTORY:
+                String newPath;
+                if (ExplorerSearcher.getLastPath().equals("/")) {
+                    newPath = ExplorerSearcher.getLastPath() + item.name;
+                } else {
+                    newPath = ExplorerSearcher.getLastPath() + "/" + item.name;
+                }
+
+                PositionManager.setPosition(ExplorerSearcher.getLastPath(), currentView.getFirstVisiblePosition());
+                PositionManager.setTop(ExplorerSearcher.getLastPath(), getCurrentViewScrollTop());
+
+                updateFileList(newPath);
+                break;
+            case IMAGE: {
+                PositionManager.setPosition(ExplorerSearcher.getLastPath(), currentView.getFirstVisiblePosition());
+                PositionManager.setTop(ExplorerSearcher.getLastPath(), getCurrentViewScrollTop());
+
+                Intent intent = new Intent(getActivity(), PhotoActivity.class);
+                intent.putExtra("name", item.name);
+                startActivity(intent);
+            }
+            break;
+            case ZIP: {
+                PositionManager.setPosition(ExplorerSearcher.getLastPath(), currentView.getFirstVisiblePosition());
+                PositionManager.setTop(ExplorerSearcher.getLastPath(), getCurrentViewScrollTop());
+
+                Intent intent = new Intent(getActivity(), ZipActivity.class);
+                intent.putExtra("path", item.path);
+                intent.putExtra("page", 0);
+                startActivity(intent);
+            }
+            break;
+        }
+    }
+
+    int getCurrentViewScrollTop() {
+        if(currentView.getChildCount() > 0) {
+            return currentView.getChildAt(0).getTop();
+        }
+        return 0;
+    }
+
+    void moveToSelection(String path) {
+        final int position = PositionManager.getPosition(path);
+        final int top = PositionManager.getTop(path);
+
+        Log.d(TAG, "updateFileList path=" + path + " position=" + position + " top="+top);
+
+        currentView.clearFocus();
+        currentView.post(new Runnable() {
+            @Override
+            public void run() {
+                currentView.requestFocusFromTouch();
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    // only for gingerbread and newer versions
+                    currentView.setSelectionFromTop(position, top);
+                } else {
+                    currentView.setSelection(position);
+                }
+
+                currentView.requestFocus();
+            }
+        });
+    }
+
+    public void updateFileList(String path) {
+        adapter.stopAllTasks();
+
+        if (BitmapCacheManager.getThumbnailCount() > MAX_THUMBNAILS) {
+            Log.d(TAG, "recycleThumbnail");
+            BitmapCacheManager.recycleThumbnail();
+        }
+
+        fileList = ExplorerSearcher.search(path);
+        if (fileList != null) {
+            adapter.setFileList(fileList);
+            adapter.notifyDataSetChanged();
+        }
+
+        textPath.setText(ExplorerSearcher.getLastPath());
+        textPath.requestLayout();
+
+        // 가장 오른쪽으로 스크롤
+        scrollPath.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                scrollPath.fullScroll(View.FOCUS_RIGHT);
+                            }
+                        }
+        );
+
+        moveToSelection(path);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                PreferenceHelper.setLastPath(getActivity(), ExplorerSearcher.getLastPath());
+            }
+        }).start();
+        refreshThumbnail(path);
+    }
+}
