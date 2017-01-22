49de06e66f2404c8943ecdce64322d585748be6d
Jungsoo Nam <namjungsoo@gmail.com>
2017-01-22 23:08:17 +0900

    tabLayout 스크롤로 변경 텍스트 작업중

---
+++ explorer/src/main/java/com/duongame/explorer/task/LoadTextTask.java
@@ -0,0 +1,124 @@
+package com.duongame.explorer.task;
+
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import org.mozilla.universalchardet.UniversalDetector;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+
+/**
+ * Created by namjungsoo on 2017. 1. 22..
+ */
+
+public class LoadTextTask extends AsyncTask<String, Integer, Void> {
+    WeakReference<TextView> weakReferenceText;
+    int page;
+    int textSize;
+    int scroll;
+    ArrayList<String> lineList;
+
+    public LoadTextTask(TextView textView, ArrayList<String> lineList, int page, int textSize, int scroll) {
+        weakReferenceText = new WeakReference<TextView>(textView);
+        this.lineList = lineList;
+        this.page = page;
+        this.textSize = textSize;
+        this.scroll = scroll;
+    }
+
+    private String checkEncoding(String fileName) {
+        byte[] buf = new byte[4096];
+        final FileInputStream fileInputStream;
+        try {
+            fileInputStream = new FileInputStream(fileName);
+
+            // (1)
+            final UniversalDetector detector = new UniversalDetector(null);
+
+            // (2)
+            int nread;
+            while ((nread = fileInputStream.read(buf)) > 0 && !detector.isDone()) {
+                detector.handleData(buf, 0, nread);
+            }
+            // (3)
+            detector.dataEnd();
+
+            // (4)
+            final String encoding = detector.getDetectedCharset();
+
+            // (5)
+            detector.reset();
+            fileInputStream.close();
+            return encoding;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected Void doInBackground(String... params) {
+        final String path = params[0];
+
+        final String encoding = checkEncoding(path);
+
+        final File file = new File(path);
+        try {
+            final FileInputStream fis = new FileInputStream(file);
+            final InputStreamReader reader = new InputStreamReader(fis, encoding);
+            final BufferedReader bufferedReader = new BufferedReader(reader);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lineList.add(line);
+
+                if (lineList.size() == (page + 1) * 1000) {
+                    publishProgress(page);
+                }
+            }
+
+            // 남은 자료가 마지막것보다 작지만 나머지를 보내야 할때는
+            final int size = lineList.size();
+            if (size > page * 1000 && size < (page + 1) * 1000) {
+                publishProgress(page);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... progress) {
+        final StringBuilder builder = new StringBuilder();
+        final int size = lineList.size();
+        if (size < page * 1000)
+            return;
+
+        int max = Math.min(size, (page + 1) * 1000);
+        for (int i = page * 1000; i < max; i++) {
+            builder.append(lineList.get(i));
+            builder.append("\n");
+        }
+        final String text = builder.toString();
+        final TextView textContent = weakReferenceText.get();
+        textContent.setText(text);
+        textContent.setTextSize(textSize);
+        textContent.setLineSpacing(0, 1.5f);
+        textContent.setTextColor(Color.BLACK);
+    }
+
+}
--- explorer/src/main/java/com/duongame/viewer/activity/TextActivity.java
+++ explorer/src/main/java/com/duongame/viewer/activity/TextActivity.java
@@ -1,39 +1,38 @@
 package com.duongame.viewer.activity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.duongame.explorer.R;
+import com.duongame.explorer.task.LoadTextTask;
 import com.duongame.viewer.listener.TextOnTouchListener;
 
-import org.mozilla.universalchardet.UniversalDetector;
+import java.util.ArrayList;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 /**
  * Created by namjungsoo on 2016-11-18.
  */
 
 // text는 pager가 아니라 상하 스크롤되는 액티비티
 public class TextActivity extends ViewerActivity {
-    ScrollView scrollText;
-    TextView textContent;
-    String path;
+    private static final String TAG = "TextActivity";
+
+    private ScrollView scrollText;
+    private TextView textContent;
+    private String path;
+    private int page;
+    private int scroll;
+    private int textSize = 20;
+    private ArrayList<String> lineList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_text);
-
-
+        
         initToolBox();
-
 
         scrollText = (ScrollView) findViewById(R.id.scroll_text);
         textContent = (TextView) findViewById(R.id.text_content);
@@ -48,31 +47,17 @@ public class TextActivity extends ViewerActivity {
     protected void processIntent() {
         final Intent intent = getIntent();
         final Bundle extras = intent.getExtras();
+
         if (extras != null) {
             path = extras.getString("path");
+            page = extras.getInt("page");
+            scroll = extras.getInt("scroll");
 
-            try {
-                File file = new File(path);
-                FileInputStream is = new FileInputStream(file);
+            textName.setText(path);
 
-                //TODO: 인코딩 검사하는데 모든 파일을 다 읽을 필요는 없다.
-                UniversalDetector detector = new UniversalDetector(null);
-                byte[] buffer = new byte[is.available()];
-                is.read(buffer);
-                is.close();
-                detector.handleData(buffer, 0, buffer.length);
-                detector.dataEnd();
-
-                String text = new String(buffer, detector.getDetectedCharset());
-                textContent.setText(text);
-                textContent.setTextSize(20);
-                textContent.setLineSpacing(0, 1.5f);
-                textContent.setTextColor(Color.BLACK);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            final LoadTextTask task = new LoadTextTask(textContent, lineList, page, textSize, scroll);
+            task.execute(path);
         }
     }
+
 }
