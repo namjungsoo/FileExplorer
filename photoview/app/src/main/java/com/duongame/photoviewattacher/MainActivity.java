58e93c63816ebb671a16e0a90d79223c04513748
Jungsoo Nam <namjungsoo@gmail.com>
2017-07-02 21:56:37 +0900

    PhotoView 테스트 프로그램

---
+++ photoview/app/src/main/java/com/duongame/photoviewattacher/MainActivity.java
@@ -0,0 +1,22 @@
+package com.duongame.photoviewattacher;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.github.chrisbanes.photoview.PhotoViewAttacher;
+
+public class MainActivity extends AppCompatActivity {
+
+    PhotoViewAttacher mAttacher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView)findViewById(R.id.tekken);
+        mAttacher = new PhotoViewAttacher(imageView);
+
+    }
+}
