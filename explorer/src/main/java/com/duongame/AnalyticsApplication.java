40bdfc8807de722664837822c9455109a40e5c16
Jungsoo Nam <namjungsoo@gmail.com>
2017-12-13 23:20:49 +0900

    Debug모드시 통계, 크래시 수집 중지

--- explorer/src/main/java/com/duongame/AnalyticsApplication.java
+++ explorer/src/main/java/com/duongame/AnalyticsApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.crash.FirebaseCrash;
 
 public class AnalyticsApplication extends Application {
     private Tracker mTracker;
@@ -18,6 +20,16 @@ public class AnalyticsApplication extends Application {
             // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
             //mTracker = analytics.newTracker(R.xml.global_tracker);
             mTracker = analytics.newTracker(R.xml.analytics_tracker);
+        }
+
+        if(BuildConfig.DEBUG) {
+            // disable FB
+            FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(false);
+            FirebaseCrash.setCrashCollectionEnabled(false);
+
+            // disable GA
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            analytics.setAppOptOut(false);
         }
         return mTracker;
     }
