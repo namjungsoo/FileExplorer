41619bc6d380560359091c2ae702aff0c0e035d8
Jungsoo Nam <namjungsoo@gmail.com>
2018-11-20 10:07:11 +0900

    좁은 리스트 작업중

---
+++ Explorer/src/main/java/com/duongame/adapter/ExplorerNarrowAdapter.java
@@ -0,0 +1,65 @@
+package com.duongame.adapter;
+
+import android.app.Activity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.duongame.R;
+import com.duongame.file.FileHelper;
+
+import java.util.ArrayList;
+
+public class ExplorerNarrowAdapter extends ExplorerAdapter {
+    public ExplorerNarrowAdapter(Activity context, ArrayList<ExplorerItem> fileList) {
+        super(context, fileList);
+    }
+
+    public void bindViewHolderExplorer(ExplorerViewHolder viewHolder, int position) {
+        if (viewHolder == null)
+            return;
+
+        if(fileList == null)
+            return;
+
+        ExplorerItem item = fileList.get(position);
+        if (item == null)
+            return;
+
+        viewHolder.name.setText(item.name);
+        viewHolder.date.setText(item.date);
+        viewHolder.size.setText(FileHelper.getMinimizedSize(item.size));
+        //viewHolder.icon.setRadiusDp(5);
+        viewHolder.iconSmall.setVisibility(View.INVISIBLE);
+        viewHolder.position = position;
+        item.position = position;
+
+        updateCheckBox(viewHolder, item);
+
+        setIconDefault(viewHolder, item);
+        setIcon(viewHolder, item);
+    }
+
+    @Override
+    public View inflateLayout(ViewGroup parent) {
+        return context.getLayoutInflater().inflate(R.layout.file_list_item, parent, false);
+    }
+
+    @Override
+    public int getFirstVisibleItem(RecyclerView recyclerView) {
+        LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+        return layoutManager.findFirstVisibleItemPosition();
+    }
+
+    @Override
+    public int getVisibleItemCount(RecyclerView recyclerView) {
+        int first = getFirstVisibleItem(recyclerView);
+
+        LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+        int last = layoutManager.findLastVisibleItemPosition();
+
+        return last - first + 1;
+    }
+
+}
