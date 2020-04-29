b45cff378ba83751b70e51ee4744b416602e572e
Jungsoo Nam <namjungsoo@gmail.com>
2020-04-30 05:54:00 +0900

    컴파일 에러로 인해 잠시 주석처리

--- Explorer/src/main/java/com/duongame/adapter/DonateOnetimeRecyclerAdapter.java
+++ Explorer/src/main/java/com/duongame/adapter/DonateOnetimeRecyclerAdapter.java
@@ -1,28 +1,28 @@
 package com.duongame.adapter;
 
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.view.ViewGroup;
-
-public class DonateOnetimeRecyclerAdapter extends RecyclerView.Adapter<DonateOnetimeRecyclerAdapter.DonateOnetimeViewHolder> {
-
-    @NonNull
-    @Override
-    public DonateOnetimeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull DonateOnetimeViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-
-    static class DonateOnetimeViewHolder {
-
-    }
-}
+//import android.support.annotation.NonNull;
+//import android.support.v7.widget.RecyclerView;
+//import android.view.ViewGroup;
+//
+//public class DonateOnetimeRecyclerAdapter extends RecyclerView.Adapter<DonateOnetimeRecyclerAdapter.DonateOnetimeViewHolder> {
+//
+//    @NonNull
+//    @Override
+//    public DonateOnetimeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull DonateOnetimeViewHolder holder, int position) {
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return 0;
+//    }
+//
+//    static class DonateOnetimeViewHolder {
+//
+//    }
+//}
