85a4c5eb9ebce36364afff33de37348850d6e5b7
Jungsoo Nam <namjungsoo@gmail.com>
2020-05-07 07:38:09 +0900

    Video pause/resume 구현 완료
    - ac3를 위해서 ffmpeg renderer 추가해야함
    - ConcatenatingMediaSource를 추가해야함 (playlist)

--- Explorer/src/main/java/com/duongame/activity/viewer/VideoActivity.kt
+++ Explorer/src/main/java/com/duongame/activity/viewer/VideoActivity.kt
@@ -16,6 +16,7 @@ import java.io.File
 
 class VideoActivity : BaseViewerActivity() {
     private lateinit var player: SimpleExoPlayer
+    private var positionMs: Long = 0L
 
     private fun buildMediaSource(path: String): MediaSource {
         val uri = Uri.fromFile(File(path))
@@ -27,23 +28,52 @@ class VideoActivity : BaseViewerActivity() {
             e.printStackTrace()
         }
         val factory: DataSource.Factory = DataSource.Factory { fileDataSource }
+//        ProgressiveMediaSource.Factory(factory)
         return ProgressiveMediaSource.Factory(factory).createMediaSource(uri)
+    }
+
+    private fun buildPlayer(): SimpleExoPlayer {
+        // simple player 만들기
+        val player = SimpleExoPlayer.Builder(this).build()
+        return player
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         contentViewResId = R.layout.activity_video
-
+        //ConcatenatingMediaSource
         super.onCreate(savedInstanceState)
-
-        // simple player 만들기
-        player = SimpleExoPlayer.Builder(this).build()
-        exoPlayerView.player = player
 
         // intent로부터 item받아서 source 처리하기
         val item: ExplorerItem = intent.extras?.get("item") as ExplorerItem
+        player = buildPlayer()
+        exoPlayerView.player = player
 
         val mediaSource = buildMediaSource(item.path)
         player.prepare(mediaSource)
         player.playWhenReady = true
     }
+
+    override fun onPause() {
+        super.onPause()
+        exoPlayerView.onPause()
+
+        player.playWhenReady = false
+        positionMs = player.currentPosition
+    }
+
+    override fun onResume() {
+        super.onResume()
+        exoPlayerView.onResume()
+
+        player.seekTo(positionMs)
+        player.playWhenReady = true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        player.stop()
+        player.release()
+    }
+
 }
